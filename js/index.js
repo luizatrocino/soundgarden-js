ac6cbe2f8d25db343af51afe295a1633d143cd7a
Luiza Trocino <luizatrocino@hotmail.com>
2022-03-28 21:45:12 -0300

    colocando try e catch

--- js/index.js
+++ js/index.js
@@ -8,32 +8,36 @@ let caminho = window.location.pathname
 if (caminho === '/listar-reserva.html'){
    
     async function listarReserva (){ 
+    
+    try{
+        var opcoesReserva = {
+            method: 'GET',
+            redirect: 'follow'
+        };
+
+        const reservas = await fetch(`${BASE_URL}/bookings`, opcoesReserva)
         
-    var opcoesReserva = {
-        method: 'GET',
-        redirect: 'follow'
-    };
+        listaReservas = await reservas.json()
 
-    const reservas = await fetch(`${BASE_URL}/bookings`, opcoesReserva)
-     
-    listaReservas = await reservas.json()
+        console.log(listaReservas)
 
-    console.log(listaReservas)
+        listaReservas.forEach ((reserva) => {
 
-    listaReservas.forEach ((reserva) => {
+        const novoPainel = `<tr>
+        <th scope="row">${reserva._id}</th>
+        <td>${reserva.owner_name}</td>
+        <td>${reserva.owner_email}</td>
+        <td>${reserva.number_tickets}</td>
+        </tr>`
 
-    const novoPainel = `<tr>
-    <th scope="row">${reserva._id}</th>
-    <td>${reserva.owner_name}</td>
-    <td>${reserva.owner_email}</td>
-    <td>${reserva.number_tickets}</td>
-    </tr>`
-
-    const table = document.querySelector('tbody')
-    table.innerHTML+=novoPainel
-    })
+        const table = document.querySelector('tbody')
+        table.innerHTML+=novoPainel
+        })
+    } catch (erro) {
+        alert(`Ops! Ocorreu um erro: \n${erro}`)
     }
 
+
+}
 listarReserva ()
 }
-
